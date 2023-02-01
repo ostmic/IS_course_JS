2e4ed64dc9a8568840662f0e8f1b2ca9980ab548
Michał Ostrowski <ostmic@gmail.com>
2023-02-01 20:53:07 +0100

    My first js app

---
+++ slider/app.js
@@ -0,0 +1,13 @@
+let person1={
+    name: "Michal",
+    lastName = "Ostrowski"
+};
+console.log(person1.name + " " person1.lastName);
+
+let person2={
+    name: "Jan",
+    lastName: "Kowalski"
+
+};
+console.log(person1.name + " " + person2.lastName);
+console.log(person1.name + " " + person2.lastName);
