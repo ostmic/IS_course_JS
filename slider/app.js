b711594ce718069e49ca1713726b460851f0f252
Michał Ostrowski <ostmic@gmail.com>
2023-02-01 21:03:52 +0100

    js object test

--- slider/app.js
+++ slider/app.js
@@ -1,13 +1,11 @@
 let person1={
     name: "Michal",
-    lastName = "Ostrowski"
+    lastName: "Ostrowski"
 };
-console.log(person1.name + " " person1.lastName);
-
 let person2={
     name: "Jan",
     lastName: "Kowalski"
 
 };
-console.log(person1.name + " " + person2.lastName);
-console.log(person1.name + " " + person2.lastName);
+console.log(person1.name + " " + person1.lastName);
+console.log(person2.name + " " + person2.lastName);
